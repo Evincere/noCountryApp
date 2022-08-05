aecd8db8075922ec8c2f722550837909fca8faca
evincere <elsemper@gmail.com>
2022-08-05 16:33:41 -0300

    first commit

---
+++ src/main/java/com/nocountry/appnocountry/AppNoCountryApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.appnocountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNoCountryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppNoCountryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nocountry/appnocountry/AppNoCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.appnocountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNoCountryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
